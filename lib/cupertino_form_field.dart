1f5bb3a9503aa26c90d0ac725f0378dcc8cb78e1
gondaimgano <gondaimgano@gmail.com>
2020-06-09 23:55:45 +0200

    added CupertinoFormField

--- lib/cupertino_form_field.dart
+++ lib/cupertino_form_field.dart
@@ -2,208 +2,73 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-
-
 class CupertinoFormField extends FormField<String> {
-  /// Creates a [FormField] that contains a [TextField].
-  ///
-  /// When a [controller] is specified, [initialValue] must be null (the
-  /// default). If [controller] is null, then a [TextEditingController]
-  /// will be constructed automatically and its `text` will be initialized
-  /// to [initialValue] or the empty string.
-  ///
-  /// For documentation about the various parameters, see the [TextField] class
-  /// and [new TextField], the constructor.
   CupertinoFormField({
-    Key key,
-    this.controller,
-    String initialValue,
-    FocusNode focusNode,
-    InputDecoration decoration = const InputDecoration(),
-    TextInputType keyboardType,
-    TextCapitalization textCapitalization = TextCapitalization.none,
-    TextInputAction textInputAction,
-    TextStyle style,
-    StrutStyle strutStyle,
-    TextDirection textDirection,
-    TextAlign textAlign = TextAlign.start,
-    TextAlignVertical textAlignVertical,
-    bool autofocus = false,
-    bool readOnly = false,
-    ToolbarOptions toolbarOptions,
-    bool showCursor,
-    bool obscureText = false,
-    bool autocorrect = true,
-    SmartDashesType smartDashesType,
-    SmartQuotesType smartQuotesType,
-    bool enableSuggestions = true,
-    bool autovalidate = false,
-    bool maxLengthEnforced = true,
-    int maxLines = 1,
-    int minLines,
-    bool expands = false,
-    int maxLength,
-    ValueChanged<String> onChanged,
-    GestureTapCallback onTap,
-    VoidCallback onEditingComplete,
-    ValueChanged<String> onFieldSubmitted,
     FormFieldSetter<String> onSaved,
     FormFieldValidator<String> validator,
-   // List<TextInputFormatter> inputFormatters,
-    bool enabled = true,
-    double cursorWidth = 2.0,
-    Radius cursorRadius,
-    Color cursorColor,
-    Brightness keyboardAppearance,
-    EdgeInsets scrollPadding = const EdgeInsets.all(20.0),
-    bool enableInteractiveSelection = true,
-    InputCounterWidgetBuilder buildCounter,
-    ScrollPhysics scrollPhysics,
+    String initialValue,
+    bool autoValidate = false,
+    bool obscureText,
+    TextInputType inputType,
+    TextEditingController controller,
     String placeholder,
-  }) : assert(initialValue == null || controller == null),
-        assert(textAlign != null),
-        assert(autofocus != null),
-        assert(readOnly != null),
-        assert(obscureText != null),
-        assert(autocorrect != null),
-        assert(enableSuggestions != null),
-        assert(autovalidate != null),
-        assert(maxLengthEnforced != null),
-        assert(scrollPadding != null),
-        assert(maxLines == null || maxLines > 0),
-        assert(minLines == null || minLines > 0),
-        assert(
-        (maxLines == null) || (minLines == null) || (maxLines >= minLines),
-        "minLines can't be greater than maxLines",
-        ),
-        assert(expands != null),
-        assert(
-        !expands || (maxLines == null && minLines == null),
-        'minLines and maxLines must be null when expands is true.',
-        ),
-        assert(!obscureText || maxLines == 1, 'Obscured fields cannot be multiline.'),
-        assert(maxLength == null || maxLength > 0),
-        assert(enableInteractiveSelection != null),
-        super(
-        key: key,
-        initialValue: controller != null ? controller.text : (initialValue ?? ''),
-        onSaved: onSaved,
-        validator: validator,
-        autovalidate: autovalidate,
-        enabled: enabled,
-        builder: (FormFieldState<String> field) {
-          final _TextFormFieldState state = field as _TextFormFieldState;
-          final InputDecoration effectiveDecoration = (decoration ?? const InputDecoration())
-              .applyDefaults(Theme.of(field.context).inputDecorationTheme);
-          void onChangedHandler(String value) {
-            if (onChanged != null) {
-              onChanged(value);
-            }
-            field.didChange(value);
-          }
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              CupertinoTextField(
-                controller: state._effectiveController,
-                onChanged: onChangedHandler,
-                minLines: 1,
-                // obscureText:obscureText??false,
-                style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  color: CupertinoColors.placeholderText,
-                  fontSize: 18,
-                ),
-                padding: EdgeInsets.all(16.0),
-                obscureText: obscureText ?? false,
-                keyboardType: keyboardType ?? TextInputType.text,
-                placeholder: placeholder,
-                decoration: BoxDecoration(
-                  color: CupertinoColors.systemGrey,
-                  borderRadius: BorderRadius.all(Radius.circular(8.0),),
-
-                  //backgroundBlendMode: BlendMode.screen
-                ),
+    this.onChanged,
+  }):super(
+      onSaved:onSaved,
+      validator:validator,
+      initialValue:  controller != null ? controller.text : (initialValue ?? ''),
+      autovalidate: autoValidate,
+      builder:(field){
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            CupertinoTextField(
+              controller: controller,
+              onChanged:onChanged,
+              minLines: 1,
+              // obscureText:obscureText??false,
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                color: CupertinoColors.placeholderText,
+                fontSize: 18,
               ),
-              SizedBox(height: 5,),
-              field.hasError?Row(
-                children: [
-                  Text(field.errorText,textAlign:TextAlign.start,style: TextStyle(color:CupertinoColors.systemRed,fontSize:11,),),
-                ],
-              ):Container(width: 0,height: 0,),
-            ],
-          );
-        },
-      );
+              padding: EdgeInsets.all(16.0),
+              obscureText: obscureText ?? false,
+              keyboardType: inputType ?? TextInputType.text,
+              placeholder: placeholder,
+             decoration: BoxDecoration(
+                color: CupertinoColors.systemGrey,
+                borderRadius: BorderRadius.all(Radius.circular(8.0),),
 
-  /// Controls the text being edited.
-  ///
-  /// If null, this widget will create its own [TextEditingController] and
-  /// initialize its [TextEditingController.text] with [initialValue].
-  final TextEditingController controller;
-
-  @override
-  _TextFormFieldState createState() => _TextFormFieldState();
-}
-
-class _TextFormFieldState extends FormFieldState<String> {
-  TextEditingController _controller;
-
-  TextEditingController get _effectiveController => widget.controller ?? _controller;
-
-  @override
-  TextFormField get widget => super.widget as TextFormField;
-
-  @override
-  void initState() {
-    super.initState();
-    if (widget.controller == null) {
-      _controller = TextEditingController(text: widget.initialValue);
-    } else {
-      widget.controller.addListener(_handleControllerChanged);
-    }
-  }
-
-  @override
-  void didUpdateWidget(TextFormField oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.controller != oldWidget.controller) {
-      oldWidget.controller?.removeListener(_handleControllerChanged);
-      widget.controller?.addListener(_handleControllerChanged);
-
-      if (oldWidget.controller != null && widget.controller == null)
-        _controller = TextEditingController.fromValue(oldWidget.controller.value);
-      if (widget.controller != null) {
-        setValue(widget.controller.text);
-        if (oldWidget.controller == null)
-          _controller = null;
+                //backgroundBlendMode: BlendMode.screen
+              ),
+            ),
+            SizedBox(height: 5,),
+            field.hasError?Row(
+              children: [
+                Text(field.errorText,textAlign:TextAlign.start,style: TextStyle(color:CupertinoColors.systemRed,fontSize:11,),),
+              ],
+            ):Container(width: 0,height: 0,),
+          ],
+        );
       }
+  );
+
+  final ValueChanged onChanged;
+
+  @override
+  FormFieldState<String> createState() =>_CupertinoFormTextField();
+}
+
+class _CupertinoFormTextField extends FormFieldState<String>{
+  @override
+  CupertinoFormField get widget => super.widget;
+  @override
+  void didChange(String value) {
+    super.didChange(value);
+    if (widget.onChanged != null) {
+      widget.onChanged(value);
     }
   }
-
-  @override
-  void dispose() {
-    widget.controller?.removeListener(_handleControllerChanged);
-    super.dispose();
-  }
-
-  @override
-  void reset() {
-    super.reset();
-    setState(() {
-      _effectiveController.text = widget.initialValue;
-    });
-  }
-
-  void _handleControllerChanged() {
-    // Suppress changes that originated from within this class.
-    //
-    // In the case where a controller has been passed in to this widget, we
-    // register this change listener. In these cases, we'll also receive change
-    // notifications for changes originating from within this class -- for
-    // example, the reset() method. In such cases, the FormField value will
-    // already have been set.
-    if (_effectiveController.text != value)
-      didChange(_effectiveController.text);
-  }
 }
+
