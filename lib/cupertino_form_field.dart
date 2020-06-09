0ba668cbee69448b65442b2806778956878ccca8
gondaimgano <gondaimgano@gmail.com>
2020-06-10 00:43:42 +0200

    added CupertinoFormField

--- lib/cupertino_form_field.dart
+++ lib/cupertino_form_field.dart
@@ -11,7 +11,7 @@ class CupertinoFormField extends FormField<String> {
     bool autoValidate = false,
     bool obscureText,
     TextInputType inputType,
-    TextEditingController controller,
+    this.controller,
     String placeholder,
     this.onChanged,
   }):super(
@@ -58,21 +58,70 @@ class CupertinoFormField extends FormField<String> {
   );
 
   final ValueChanged onChanged;
-
+  final TextEditingController controller;
   @override
   FormFieldState<String> createState() =>_CupertinoFormTextField();
 }
 
 class _CupertinoFormTextField extends FormFieldState<String>{
+  TextEditingController _controller;
+
+  TextEditingController get _effectiveController => widget.controller ?? _controller;
+
   @override
-  CupertinoFormField get widget => super.widget;
+  CupertinoFormField get widget => super.widget as CupertinoFormField;
+
   @override
-  void didChange(String value) {
-    print(value);
-    super.didChange(value);
-    if (widget.onChanged != null) {
-      widget.onChanged(value);
+  void initState() {
+    super.initState();
+    if (widget.controller == null) {
+      _controller = TextEditingController(text: widget.initialValue);
+    } else {
+      widget.controller.addListener(_handleControllerChanged);
     }
+  }
+
+  @override
+  void didUpdateWidget(TextFormField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.controller != oldWidget.controller) {
+      oldWidget.controller?.removeListener(_handleControllerChanged);
+      widget.controller?.addListener(_handleControllerChanged);
+
+      if (oldWidget.controller != null && widget.controller == null)
+        _controller = TextEditingController.fromValue(oldWidget.controller.value);
+      if (widget.controller != null) {
+        setValue(widget.controller.text);
+        if (oldWidget.controller == null)
+          _controller = null;
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.controller?.removeListener(_handleControllerChanged);
+    super.dispose();
+  }
+
+  @override
+  void reset() {
+    super.reset();
+    setState(() {
+      _effectiveController.text = widget.initialValue;
+    });
+  }
+
+  void _handleControllerChanged() {
+    // Suppress changes that originated from within this class.
+    //
+    // In the case where a controller has been passed in to this widget, we
+    // register this change listener. In these cases, we'll also receive change
+    // notifications for changes originating from within this class -- for
+    // example, the reset() method. In such cases, the FormField value will
+    // already have been set.
+    if (_effectiveController.text != value)
+      didChange(_effectiveController.text);
   }
 }
 
