502d75e8f58c087e313c51aad2caa885bfc7add5
gondaimgano <gondaimgano@gmail.com>
2020-08-07 15:55:30 +0200

    popUpDate field

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gondaiuipackage/gondaiuipackage.dart';
-import 'package:neumorphic/neumorphic.dart';
 
 void main() {
   runApp(MyApp());
